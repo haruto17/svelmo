91d8856fa965fc34e10c43bf836fa704e98665ae
haruto17 <s1902272@sendai-nct.jp>
2023-07-26 19:51:21 +0900

    modalの表示

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-  function handleClick() {
-    console.log("click!!");
-  }
+  let isModalOpen = false;
 </script>
 
 <main>
-  <button class="btn btn-secondary" on:click={handleClick}>Button</button>
+  <button
+    class="btn btn-secondary bg-green-500"
+    on:click={() => (isModalOpen = true)}>add</button
+  >
+  <dialog class="modal" class:modal-open={isModalOpen}>
+    <form method="dialog" class="modal-box">
+      <h3 class="font-bold text-lg">Hello!</h3>
+      <p>This is modal!!</p>
+      <div class="modal-action">
+        <button class="btn" on:click={() => (isModalOpen = false)}>Close</button
+        >
+      </div>
+    </form>
+  </dialog>
 </main>
+
+<style></style>
