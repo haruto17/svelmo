9d52a3c432be385d439853e16871f90cb1fa1871
haruto17 <s1902272@sendai-nct.jp>
2023-07-26 19:58:22 +0900

    modalにテキストフィールドを追加

--- src/App.svelte
+++ src/App.svelte
@@ -9,8 +9,27 @@
   >
   <dialog class="modal" class:modal-open={isModalOpen}>
     <form method="dialog" class="modal-box">
+      <button
+        class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+        on:click={() => (isModalOpen = false)}>✕</button
+      >
       <h3 class="font-bold text-lg">Hello!</h3>
       <p>This is modal!!</p>
+      <input
+        type="text"
+        placeholder="Title"
+        class="input input-bordered w-full max-w-xs"
+      />
+      <input
+        type="text"
+        placeholder="Contents"
+        class="input input-bordered w-full max-w-xs"
+      />
+      <input
+        type="text"
+        placeholder="Tags"
+        class="input input-bordered w-full max-w-xs"
+      />
       <div class="modal-action">
         <button class="btn" on:click={() => (isModalOpen = false)}>Close</button
         >
