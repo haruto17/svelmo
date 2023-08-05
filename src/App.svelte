ec8033525f9cddf0a29495f61b7c00a134e5c45d
haruto17 <s1902272@sendai-nct.jp>
2023-08-05 21:21:53 +0900

    ボタンを押したときに<Category />の追加

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
   import Category from "./lib/Category.svelte";
+  import { onMount } from "svelte";
+
+  let categoryContainer;
+
+  function addCategory() {
+    new Category({ target: document.getElementById("categoryList") });
+  }
 </script>
 
 <main>
-  <div class="flex flex-row">
-    <Category />
-    <Category />
-    <Category />
-    <Category />
-    <Category />
-    <Category />
-    <Category />
-    <Category />
+  <div id="categoryList" class="flex flex-row" bind:this={categoryContainer}>
     <Category />
   </div>
+  <button class="btn btn-accent" on:click={addCategory}> + </button>
 </main>
 
 <style></style>
